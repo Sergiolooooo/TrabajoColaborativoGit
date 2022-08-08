44c27a1854fe0b23326fb97fbdd53a6ac3324116
Mariana <mapachecosa@est.utn.ac.cr>
2022-08-08 15:49:56 -0600

    se crea el repositorio.

---
+++ src/trabajocolaborativogit/TrabajoColaborativoGit.java
@@ -0,0 +1,17 @@
+
+package trabajocolaborativogit;
+
+/**
+ *
+ * @author ADMIN
+ */
+public class TrabajoColaborativoGit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
